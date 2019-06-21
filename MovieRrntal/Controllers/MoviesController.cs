f483fa98c5787731e0d8cce78725f769ad089a45
leonardoobay <leonardoobay@gmail.com>
2019-06-20 20:55:10 -0700

    Add project files.

---
+++ MovieRrntal/Controllers/MoviesController.cs
@@ -0,0 +1,19 @@
+﻿using MovieRrntal.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MovieRrntal.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek is Love Shrek is Life" };
+            return View(movie);
+        }
+    }
+}
---
+++ MovieRrntal/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MovieRrntal.MvcApplication" Language="C#" %>
